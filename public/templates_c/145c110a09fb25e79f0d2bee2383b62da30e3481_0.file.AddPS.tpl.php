d5f40966f882d5e23a098738e1f11c0c6c54eae8
Eryk Tyndel <56363275+i3loodyShadow@users.noreply.github.com>
2020-09-30 18:43:21 +0200

    Create 145c110a09fb25e79f0d2bee2383b62da30e3481_0.file.AddPS.tpl.php

---
+++ public/templates_c/145c110a09fb25e79f0d2bee2383b62da30e3481_0.file.AddPS.tpl.php
@@ -0,0 +1,110 @@
+<?php
+/* Smarty version 3.1.30, created on 2020-09-30 12:40:31
+  from "D:\Xampp\htdocs\Projekt_Sklepu\app\views\AddPS.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_5f74609f952c06_55939776',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '145c110a09fb25e79f0d2bee2383b62da30e3481' => 
+    array (
+      0 => 'D:\\Xampp\\htdocs\\Projekt_Sklepu\\app\\views\\AddPS.tpl',
+      1 => 1601412089,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:main.tpl' => 1,
+    'file:messages.tpl' => 1,
+  ),
+),false)) {
+function content_5f74609f952c06_55939776 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, true);
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_13943777265f74609f951f74_05680429', 'content');
+?>
+
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_12530545f74609f9528e2_33093211', 'footer');
+$_smarty_tpl->inheritance->endChild();
+$_smarty_tpl->_subTemplateRender("file:main.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 2, false);
+}
+/* {block 'content'} */
+class Block_13943777265f74609f951f74_05680429 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+<div class="pure-menu pure-menu-horizontal bottom-margin">
+        <a href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_url;?>
+backToAdminNavigator"}Opcje_administratora  class="pure-menu-heading pure-menu-link">Powrót do administracyjnego nawigatora</a>
+</div>
+    
+<p>
+<div class="pure-g">
+<div class="l-box-lrg pure-u-1 pure-u-med-1">
+        
+<form action="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_root;?>
+PSSave" method="post"  class="pure-form pure-form-aligned">
+	<fieldset>
+        <div class="l-box-lrg pure-u-1 pure-u-med-5-24">
+		<label for="producent">Wpisz producenta</label>
+                <input id="producent" type="text" placeholder="Producent" name="producent" value="<?php echo $_smarty_tpl->tpl_vars['form']->value->producent;?>
+">
+        </div>
+        <div class="l-box-lrg pure-u-1 pure-u-med-4-24">
+		<label for="model">Wpisz model</label>
+                <input id="model" type="text" placeholder="Model" name="model" value="<?php echo $_smarty_tpl->tpl_vars['form']->value->model;?>
+">
+	</div>
+        <div class="l-box-lrg pure-u-1 pure-u-med-5-24">
+		<label for="moc">Wpisz moc</label>
+                <input id="moc" type="text" placeholder="Moc" name="moc" value="<?php echo $_smarty_tpl->tpl_vars['form']->value->moc;?>
+">
+	</div>
+        <div class="l-box-lrg pure-u-1 pure-u-med-5-24">
+		<label for="cena">Wpisz cenę</label>
+                <input id="cena" type="text" placeholder="Cena" name="cena" value="<?php echo $_smarty_tpl->tpl_vars['form']->value->cena;?>
+">
+	</div>
+
+        <div class="l-box-lrg pure-u-1 pure-u-med-1">
+		<input type="submit" value="Zapisz" class="pure-button"/>
+        </div>
+	</fieldset>
+        <input type="hidden" name="id" value="<?php echo $_smarty_tpl->tpl_vars['form']->value->id;?>
+">
+</form>
+</div>
+
+<div class="l-box-lrg pure-u-1 pure-u-med-1">
+<?php $_smarty_tpl->_subTemplateRender("file:messages.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+
+</div>
+
+</div>
+<?php
+}
+}
+/* {/block 'content'} */
+/* {block 'footer'} */
+class Block_12530545f74609f9528e2_33093211 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+Panel administracyjny.<?php
+}
+}
+/* {/block 'footer'} */
+}
