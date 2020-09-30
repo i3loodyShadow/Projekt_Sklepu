36f12be93177372c1d5aa9ca1bbb239a579db1e4
Eryk Tyndel <56363275+i3loodyShadow@users.noreply.github.com>
2020-09-30 18:31:19 +0200

    Create AddProcessor.class.php

---
+++ app/forms/AddProcessor.class.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace app\forms;
+
+class AddProcessor {
+    public $id;
+    public $producent;
+    public $model;
+    public $taktowanie;
+    public $lRdzeni;
+    public $cena;
+}
