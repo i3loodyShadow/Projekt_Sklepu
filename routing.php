9d2bbda338c54a7b41d1b612e0abde43a6d91fbb
Eryk Tyndel <56363275+i3loodyShadow@users.noreply.github.com>
2020-09-30 18:44:34 +0200

    Create routing.php

---
+++ routing.php
@@ -0,0 +1,56 @@
+<?php
+
+use core\App;
+use core\Utils;
+
+App::getRouter()->setDefaultRoute('NavigateShow'); // akcja/ścieżka domyślna
+App::getRouter()->setLoginRoute('Login'); // akcja/ścieżka na potrzeby logowania (przekierowanie, gdy nie ma dostępu)
+
+Utils::addRoute('Register', 'RegisterCtrl');
+Utils::addRoute('Login', 'LoginCtrl');
+Utils::addRoute('Logout', 'LoginCtrl');
+
+Utils::addRoute('NavigateShow', 'NavigateCtrl');
+Utils::addRoute('NavigateLogIn', 'NavigateCtrl');
+Utils::addRoute('NavigateLogOut', 'NavigateCtrl');
+Utils::addRoute('backFromService', 'NavigateCtrl');
+
+Utils::addRoute('ProductView', 'ProductCtrl');
+Utils::addRoute('ProductPartView', 'ProductCtrl');
+Utils::addRoute('productEdit', 'ProductCtrl',['admin']);
+Utils::addRoute('productDelete', 'ProductCtrl',['admin']);
+
+Utils::addRoute('AdminView', 'AdminCtrl', ['admin']);
+Utils::addRoute('backToAdminNavigator', 'AdminCtrl', ['admin']);
+
+Utils::addRoute('addToShoppingCart', 'ProductCtrl', ['user','admin']);
+Utils::addRoute('ShoppingCartView', 'ShoppingCartCtrl', ['user','admin']);
+Utils::addRoute('deleteFromSC', 'ShoppingCartCtrl', ['user','admin']);
+
+Utils::addRoute('NewProcessor', 'ProcessorEditCtrl', ['admin']);
+Utils::addRoute('processorSave', 'ProcessorEditCtrl', ['admin']);
+Utils::addRoute('processorEdit', 'ProcessorEditCtrl', ['admin']);
+
+Utils::addRoute('NewGC', 'GCEditCtrl', ['admin']);
+Utils::addRoute('GCSave', 'GCEditCtrl', ['admin']);
+Utils::addRoute('GCEdit', 'GCEditCtrl', ['admin']);
+
+Utils::addRoute('NewMoBo', 'MoBoEditCtrl', ['admin']);
+Utils::addRoute('MoBoSave', 'MoBoEditCtrl', ['admin']);
+Utils::addRoute('MoBoEdit', 'MoBoEditCtrl', ['admin']);
+
+Utils::addRoute('NewRAM', 'RAMEditCtrl', ['admin']);
+Utils::addRoute('RAMSave', 'RAMEditCtrl', ['admin']);
+Utils::addRoute('RAMEdit', 'RAMEditCtrl', ['admin']);
+
+Utils::addRoute('NewHD', 'HDEditCtrl', ['admin']);
+Utils::addRoute('HDSave', 'HDEditCtrl', ['admin']);
+Utils::addRoute('HDEdit', 'HDEditCtrl', ['admin']);
+
+Utils::addRoute('NewPS', 'PSEditCtrl', ['admin']);
+Utils::addRoute('PSSave', 'PSEditCtrl', ['admin']);
+Utils::addRoute('PSEdit', 'PSEditCtrl', ['admin']);
+
+Utils::addRoute('NewCase', 'CaseEditCtrl', ['admin']);
+Utils::addRoute('CaseSave', 'CaseEditCtrl', ['admin']);
+Utils::addRoute('CaseEdit', 'CaseEditCtrl', ['admin']);
