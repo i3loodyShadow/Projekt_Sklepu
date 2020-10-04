a3609db474073ca8f21259f526692b1047a0d5b6
Eryk Tyndel <56363275+i3loodyShadow@users.noreply.github.com>
2020-10-04 19:33:50 +0200

    Update NavigateCtrl.class.php

--- app/controllers/NavigateCtrl.class.php
+++ app/controllers/NavigateCtrl.class.php
@@ -4,35 +4,34 @@ namespace app\controllers;
 
 use core\App;
 use core\Utils;
+use core\SessionUtils;
 
 class NavigateCtrl {
+           
+    public function action_backFromService(){
+        Utils::addInfoMessage('Witamy ponownie w naszym sklepie!');
+        $this->generateText();
+    }
+
+    public function action_NavigateShow(){
+        Utils::addInfoMessage('Witamy w naszym sklepie!');
+        $this->generateText();
+    }
+
+    public function action_NavigateLogIn(){
+        Utils::addInfoMessage('Witamy po zalogowaniu!');
+        $this->generateText();
+    }
+
+    public function action_NavigateLogOut() {
+        Utils::addInfoMessage('Do zobaczenia!');
+        $this->generateText();  
+    }
+
+    public function generateText(){		
+        App::getSmarty()->assign('page_title','Twoje części komputerowe');
+        App::getSmarty()->assign('page_description','Proszę nacisnąć przycisk poniżej aby przejść do wyboru komponentów');
         
-        public function action_backFromService(){
-                Utils::addInfoMessage('Witamy ponownie w naszym sklepie!');
-                $this->generateText();
-                App::getSmarty()->display('NavigateView.tpl');
-        }
-        
-	public function action_NavigateShow(){
-            Utils::addInfoMessage('Witamy w naszym sklepie!');
-            $this->generateText();
-            App::getSmarty()->display('NavigateView.tpl');
-	}
-        
-        public function action_NavigateLogIn(){
-                Utils::addInfoMessage('Witamy po zalogowaniu!');
-                $this->generateText();
-                App::getSmarty()->display('NavigateView.tpl');
-	}
-        
-        public function action_NavigateLogOut() {
-                Utils::addInfoMessage('Do zobaczenia!');
-                $this->generateText();
-                App::getSmarty()->display('NavigateView.tpl');
-        }
-	
-	public function generateText(){		
-            App::getSmarty()->assign('page_title','Twoje części komputerowe');
-            App::getSmarty()->assign('page_description','Proszę nacisnąć przycisk poniżej aby przejść do wyboru komponentów');
-	}
+        App::getSmarty()->display('NavigateView.tpl');
+    }
 }
