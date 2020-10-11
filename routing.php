f6363ef9b3a55aef224a2e9bacb1b51dd81d5054
Eryk Tyndel <56363275+i3loodyShadow@users.noreply.github.com>
2020-10-11 18:46:42 +0200

    Update routing.php

--- routing.php
+++ routing.php
@@ -17,7 +17,15 @@ Utils::addRoute('backFromService', 'NavigateCtrl');
 
 Utils::addRoute('ProductView', 'ProductCtrl');
 Utils::addRoute('ProductPartView', 'ProductCtrl');
-Utils::addRoute('productEdit', 'ProductCtrl',['admin']);
+Utils::addRoute('ProductEdition', 'ProductCtrl');
+Utils::addRoute('ProductDetails', 'ProductCtrl');
+Utils::addRoute('productDetails', 'ProductDetailsCtrl');
+
+Utils::addRoute('NewProduct', 'ProductNewCtrl');
+Utils::addRoute('ProductSave', 'ProductNewCtrl');
+
+Utils::addRoute('addParameter', 'ProductEditCtrl');
+Utils::addRoute('productEdit', 'ProductEditCtrl',['admin']);
 Utils::addRoute('productDelete', 'ProductCtrl',['admin']);
 
 Utils::addRoute('passEdit', 'AccountCtrl',['user','admin']);
@@ -34,31 +42,3 @@ Utils::addRoute('backToAdminNavigator', 'AdminCtrl', ['admin']);
 Utils::addRoute('addToShoppingCart', 'ProductCtrl', ['user']);
 Utils::addRoute('ShoppingCartView', 'ShoppingCartCtrl', ['user']);
 Utils::addRoute('deleteFromSC', 'ShoppingCartCtrl', ['user']);
-
-Utils::addRoute('NewProcessor', 'ProcessorEditCtrl', ['admin']);
-Utils::addRoute('processorSave', 'ProcessorEditCtrl', ['admin']);
-Utils::addRoute('processorEdit', 'ProcessorEditCtrl', ['admin']);
-
-Utils::addRoute('NewGC', 'GCEditCtrl', ['admin']);
-Utils::addRoute('GCSave', 'GCEditCtrl', ['admin']);
-Utils::addRoute('GCEdit', 'GCEditCtrl', ['admin']);
-
-Utils::addRoute('NewMoBo', 'MoBoEditCtrl', ['admin']);
-Utils::addRoute('MoBoSave', 'MoBoEditCtrl', ['admin']);
-Utils::addRoute('MoBoEdit', 'MoBoEditCtrl', ['admin']);
-
-Utils::addRoute('NewRAM', 'RAMEditCtrl', ['admin']);
-Utils::addRoute('RAMSave', 'RAMEditCtrl', ['admin']);
-Utils::addRoute('RAMEdit', 'RAMEditCtrl', ['admin']);
-
-Utils::addRoute('NewHD', 'HDEditCtrl', ['admin']);
-Utils::addRoute('HDSave', 'HDEditCtrl', ['admin']);
-Utils::addRoute('HDEdit', 'HDEditCtrl', ['admin']);
-
-Utils::addRoute('NewPS', 'PSEditCtrl', ['admin']);
-Utils::addRoute('PSSave', 'PSEditCtrl', ['admin']);
-Utils::addRoute('PSEdit', 'PSEditCtrl', ['admin']);
-
-Utils::addRoute('NewCase', 'CaseEditCtrl', ['admin']);
-Utils::addRoute('CaseSave', 'CaseEditCtrl', ['admin']);
-Utils::addRoute('CaseEdit', 'CaseEditCtrl', ['admin']);
