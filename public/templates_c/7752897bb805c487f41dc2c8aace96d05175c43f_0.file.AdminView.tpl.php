77ab468cabcac52d07db9f81649f48d7eb557407
Eryk Tyndel <56363275+i3loodyShadow@users.noreply.github.com>
2020-10-11 18:47:04 +0200

    Update 7752897bb805c487f41dc2c8aace96d05175c43f_0.file.AdminView.tpl.php

--- public/templates_c/7752897bb805c487f41dc2c8aace96d05175c43f_0.file.AdminView.tpl.php
+++ public/templates_c/7752897bb805c487f41dc2c8aace96d05175c43f_0.file.AdminView.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2020-09-30 12:40:28
+/* Smarty version 3.1.30, created on 2020-10-11 17:22:40
   from "D:\Xampp\htdocs\Projekt_Sklepu\app\views\AdminView.tpl" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_5f74609ca27cc0_54627724',
+  'unifunc' => 'content_5f832340276f44_30277204',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '7752897bb805c487f41dc2c8aace96d05175c43f' => 
     array (
       0 => 'D:\\Xampp\\htdocs\\Projekt_Sklepu\\app\\views\\AdminView.tpl',
-      1 => 1601462426,
+      1 => 1602429758,
       2 => 'file',
     ),
   ),
@@ -22,19 +22,19 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:messages.tpl' => 1,
   ),
 ),false)) {
-function content_5f74609ca27cc0_54627724 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5f832340276f44_30277204 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_loadInheritance();
 $_smarty_tpl->inheritance->init($_smarty_tpl, true);
 ?>
 
 
 <?php 
-$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_14249885375f74609ca27820_88986810', 'content');
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_12989589425f832340276995_88144581', 'content');
 $_smarty_tpl->inheritance->endChild();
 $_smarty_tpl->_subTemplateRender("file:main.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 2, false);
 }
 /* {block 'content'} */
-class Block_14249885375f74609ca27820_88986810 extends Smarty_Internal_Block
+class Block_12989589425f832340276995_88144581 extends Smarty_Internal_Block
 {
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
 ?>
@@ -53,31 +53,18 @@ public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
 </div>
 
 <div class="l-box-lrg pure-u-1 pure-u-med-14-24">
-    <div class="pure-menu-horizontal">
-        <li class="pure-menu-selected"><a class="pure-button" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_url;?>
+    <div style="text-align:center">
+        <a align="center" class="pure-button" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_url;?>
+NewProduct">Dodaj produkt</a>
+    </div>  
+    &nbsp;
+    <div style="text-align:center">
+        <a align="center" class="pure-button" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_url;?>
 ProductView">Podgląd wszystkich produktów</a>
-        <li></li>
-        <li class="pure-menu-selected"><a class="pure-button" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_url;?>
-NewProcessor">Dodaj procesor</a>
-        <li></li>
-        <li class="pure-menu-selected"><a class="pure-button" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_url;?>
-NewGC">Dodaj Kartę Graficzną</a>
-        <li></li>
-        <li class="pure-menu-selected"><a class="pure-button" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_url;?>
-NewMoBo">Dodaj Płytę Główną</a>
-        <li></li>
-        <p><li class="pure-menu-selected"><a class="pure-button" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_url;?>
-NewRAM">Dodaj Pamięć RAM</a>
-        <li></li>
-        <li class="pure-menu-selected"><a class="pure-button" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_url;?>
-NewHD">Dodaj Dysk</a>    
-        <li></li>
-        <li class="pure-menu-selected"><a class="pure-button" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_url;?>
-NewPS">Dodaj Zasilacz</a>
-        <li></li>
-        <li class="pure-menu-selected"><a class="pure-button" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_url;?>
-NewCase">Dodaj Obudowę</a>
     </div>
+    
+
+
 </div>
 
 </div>
